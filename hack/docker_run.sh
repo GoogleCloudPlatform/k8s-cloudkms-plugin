23f11aebde536ec8113d61810155f1ecff27d0c1
immutablet <alextc@google.com>
2018-05-18 15:08:14 -0700

    Remove volume mount from docker test

--- hack/docker_run.sh
+++ hack/docker_run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
+IMAGE='gcr.io/google-containers/k8s-cloud-kms-plugin-test:v0.1.1'
+KEY='projects/cloud-kms-lab/locations/us-central1/keyRings/ring-01/cryptoKeys/key-01'
+SOCKET='@kms-plugin-socket'
+HEALTHZ_PORT=8081
+
 # Starting KMS via docker
-docker run -it -p 8081:8081 \
-           -v /var/run/kmsplugin \
-           gcr.io/google-containers/k8s-cloud-kms-plugin-test:v0.1.1 /k8s-cloud-kms-plugin \
-           --logtostderr \
-           --path-to-unix-socket=@kms-plugin-socket \
-           --key-uri=projects/cloud-kms-lab/locations/us-central1/keyRings/ring-01/cryptoKeys/key-01
+docker run -it -p ${HEALTHZ_PORT}:${HEALTHZ_PORT} ${IMAGE} /k8s-cloud-kms-plugin --logtostderr --path-to-unix-socket=${SOCKET} --key-uri=${KEY}
