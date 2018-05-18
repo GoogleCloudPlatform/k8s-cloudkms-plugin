6234f7773a5cca6334c4e7350ea9711c9b049a2e
immutablet <alextc@google.com>
2018-05-18 15:17:38 -0700

    Add metrics port to docker test run.

--- hack/docker_run.sh
+++ hack/docker_run.sh
@@ -4,6 +4,8 @@ IMAGE='gcr.io/google-containers/k8s-cloud-kms-plugin-test:v0.1.1'
 KEY='projects/cloud-kms-lab/locations/us-central1/keyRings/ring-01/cryptoKeys/key-01'
 SOCKET='@kms-plugin-socket'
 HEALTHZ_PORT=8081
+METRICS_PORT=8082
 
 # Starting KMS via docker
-docker run -it -p ${HEALTHZ_PORT}:${HEALTHZ_PORT} ${IMAGE} /k8s-cloud-kms-plugin --logtostderr --path-to-unix-socket=${SOCKET} --key-uri=${KEY}
+docker run -it --rm -p ${HEALTHZ_PORT}:${HEALTHZ_PORT} -p ${METRICS_PORT}:${METRICS_PORT} ${IMAGE} \
+ /k8s-cloud-kms-plugin --logtostderr --path-to-unix-socket=${SOCKET} --key-uri=${KEY}
